7c179625c06ae22f75d74f1a79047348fd2fbfd6
dagilleland <dagilleland@shaw.ca>
2017-01-08 11:02:58 -0700

    place for future unit tests

---
+++ specs/spec.markdown.js
@@ -0,0 +1,4 @@
+// TODO: Tests for custom markdown renderings
+// - customLinkRenderer: Fix relative links to work from the site root
+// - customImageRenderer: Fix relative links to work from the site root
+// - customHeadingRenderer: Give headings an achor
