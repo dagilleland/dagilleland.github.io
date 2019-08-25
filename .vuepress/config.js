858c0a68ecd23a7905c76255e5e738ee9438113c
Dan Gilleland <dagilleland@shaw.ca>
2019-08-25 10:33:44 -0600

    VuePress upgrade to 1.x
    Also switched from npm to yarn package management

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,20 +1,18 @@
 module.exports = {
   title: 'Gilleland.ca',
   description: 'Personal and Professional Blog of Dan Gilleland',
-  markdown: {
-    config: md => {
-      md.set({ breaks: true })
-      md.use(require('markdown-it-imsize'))
-      md.use(require('markdown-it-mermaid').default) // leave default options
-      md.use(require('markdown-it-checkbox'))
-      md.use(require('markdown-it-kbd'))
-      md.use(require('markdown-it-deflist'))
-      md.use(require('markdown-it-abbr'))
-      md.use(require('markdown-it-mark'))
-      md.use(require('markdown-it-footnote'))
-      md.use(require('markdown-it-sub'))
-      md.use(require('markdown-it-sup'))
-    }
+  extendMarkdown(md) {
+    md.set({ breaks: true })
+    md.use(require('markdown-it-imsize'))
+    md.use(require('markdown-it-mermaid').default) // leave default options
+    md.use(require('markdown-it-checkbox'))
+    md.use(require('markdown-it-kbd'))
+    md.use(require('markdown-it-deflist'))
+    md.use(require('markdown-it-abbr'))
+    md.use(require('markdown-it-mark'))
+    md.use(require('markdown-it-footnote'))
+    md.use(require('markdown-it-sub'))
+    md.use(require('markdown-it-sup'))
   },
   themeConfig: {
     nav: [
@@ -43,6 +41,18 @@ module.exports = {
       '/': [
         ''
       ]
+    }
+  },
+  plugins: {
+    '@vuewpress/pwa': {
+      serviceWorker: true,
+      updatePopup: true
+    },
+    '@vuepress/active-header-links': { },
+    '@vuepress/back-to-top': { },
+    '@vuepress/last-updated': { },
+    '@vuepress/blog' : {
+      
     }
   }
 }
