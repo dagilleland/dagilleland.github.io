52635f06a7b7ce7acad3f574ffe65df9852ca803
Dan Gilleland <dagilleland@shaw.ca>
2020-04-12 21:24:28 -0600

    Add @vuepress/plugin-blog
    See if it goes with the theme

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -76,6 +76,7 @@ module.exports = {
     },
     '@vuepress/active-header-links': { },
     '@vuepress/back-to-top': { },
-    '@vuepress/last-updated': { }
+    '@vuepress/last-updated': { },
+    '@vuepress/blog': { }
   }
 }
