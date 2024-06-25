1c361c8e0fc5d118209e6ddfe6f055c0b0d5edf8
Dan Gilleland <dagilleland@shaw.ca>
2024-06-25 05:49:32 -0600

    Rudementary content for initial publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,28 +5,51 @@ import starlightBlog from 'starlight-blog'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-			plugins: [starlightBlog()],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+    site: 'https://gilleland.ca',
+    integrations: [
+        starlight({
+            title: 'Gilleland.ca',
+            social: {
+                github: 'https://github.com/dagilleland/dagilleland.github.io',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        // Each item here is one entry in the navigation menu.
+                        { label: 'Example Guide', link: '/guides/example/' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Tour',
+                    items: [
+                        { label: 'Site Tour', link: '/tour/' },
+                    ],
+                }
+            ],
+            customCss: ['./src/tailwind.css'],
+            plugins: [starlightBlog({
+                // Values here are the defaults, unless otherwise specified.
+                title: 'Blog',
+                postCount: 5,
+                recentPostCount: 10,
+                authors: {
+                    dan: {
+                        name: 'Dan Gilleland',
+                        title: 'Owner of Gilleland.ca',
+                        picture: '/DanGilleland.jpg',
+                        url: 'https://gilleland.ca',
+                    }
+                },
+                prevNextLinksOrder: 'reverse-chronological',
+                prefix: 'blog', // BTW, this means "look under src/content/blog",
+
+            })],
+        }),
+        tailwind({ applyBaseStyles: false }),
+    ],
 });
